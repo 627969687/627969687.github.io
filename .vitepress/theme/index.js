248a72dc44232d870ae03c3c93166ed94676761c
TkJacky <>
2025-01-09 00:17:29 +0800

    1. 修改侧边栏css文件名 2. 增加点击图片放大功能

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,33 @@
-import DefaultTheme from 'vitepress/theme';
-import './custom.css';
+import DefaultTheme from 'vitepress/theme'
+// 点击图片放大
+import { onMounted, watch, nextTick } from 'vue' 
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+// 侧边栏 css
+import './sideBar.css'
+// 点击图片放大css
+import './imgZoom.css'
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  
+  // 点击图片放大
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // 为所有图片增加缩放功能
+      // mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+        mediumZoom(".content-container p img", {
+	        background: "var(--vp-c-bg)",
+	        container: document.body,
+		});
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  }
+}
